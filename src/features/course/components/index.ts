35e369f60940209418dbed6bd6f4af4cc495e8e7
222112243 <222112243@stis.ac.id>
2025-10-29 23:14:35 +0700

    fix: update export statements in course components for consistency

--- src/features/course/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { CourseLayout } from './CourseLayout';
-export { CourseHeader } from './CourseHeader';
-export { CourseFilters } from './CourseFilters';
-export { CourseCard } from './CourseCard';
-export { CourseGrid } from './CourseGrid';
-export { ViewModeToggle } from './ViewModeToggle';
--- src/features/course/components/index.tsx
+++ src/features/course/components/index.tsx
@@ -1,5 +1,6 @@
-export { CourseLayout } from './CourseLayout';
-export { CourseHeader } from './CourseHeader';
-export { CourseFilters } from './CourseFilters';
-export { CourseCard } from './CourseCard';
-export { CourseGrid } from './CourseGrid';
+export { CourseLayout } from "./CourseLayout";
+export { CourseHeader } from "./CourseHeader";
+export { CourseFilters } from "./CourseFilters";
+export { CourseCard } from "./CourseCard";
+export { CourseGrid } from "./CourseGrid";
+export { ViewModeToggle } from "./ViewModeToggle";
