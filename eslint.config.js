9a43f58ee3ba7447501a4b9505529587b0acb827
falanarh <falanarofako29@gmail.com>
2025-11-20 22:01:32 +0700

    feat(eslint): add nextjs eslint plugin
    - Added pluginNext to eslint config
    - Updated package.json and lockfile

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 const js = require('@eslint/js');
 const tseslint = require('typescript-eslint');
+const pluginNext = require('@next/eslint-plugin-next');
 
 module.exports = [
   js.configs.recommended,
   ...tseslint.configs.recommended,
+  pluginNext.configs.recommended,
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
